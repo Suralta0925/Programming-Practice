82d01d3033057e90274b204acef20461ea425cdc
vince <vinceiansuralta3@gmail.com>
2025-07-30 20:39:44 +0800

    Started react learning

---
+++ React/reactPractice/my-react-app/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './Header.tsx'
+import Footer from './Footer.tsx'
+import Food from './Food.tsx'
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+      
+    </>
+  );
+}
+
+export default App
